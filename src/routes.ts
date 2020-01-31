0fbe4f7f4aa362972a4d0e8c52ff2a93c574b85c
imasr <ashishs723@gmail.com>
2020-01-31 18:53:30 +0530

    changes

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,13 @@
+import express from "express";
 import { userRouter } from "./modules/user/user.route";
 
 export const routes = [
   {
+    path: "",
+    handler: express.Router().get('', (req, res) => {
+      res.sendFile(__dirname + '/assets/public/index.html');
+    })
+  }, {
     path: "/api/users",
     handler: userRouter
   }
